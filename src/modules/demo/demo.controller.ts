2265bd883fbb66dc694c2d24190754a3ab6f5b2e
Luka <luka@pop-os.localdomain>
2019-11-18 15:44:13 +0100

    update demo file

--- src/modules/demo/demo.controller.ts
+++ src/modules/demo/demo.controller.ts
@@ -7,7 +7,10 @@ export class DemoController {
     return {
       clani: ['lb5480', 'kv8904'],
       opis_projekta: 'Projekt implementira aplikacijo za deljenje glasbe.',
-      mikrostoritve: ['http://157.230.124.197:3000/v1/catalog'],
+      mikrostoritve: [
+        'http://157.230.124.197:3000/v1/catalog',
+        'http://46.101.213.29:3000/v1/comment/1',
+      ],
       github: [
         'https://github.com/RSO-lb-kv/music-catalog-ms',
         'https://github.com/RSO-lb-kv/song-upload-ms',
@@ -16,10 +19,12 @@ export class DemoController {
       travis: [
         'https://travis-ci.org/RSO-lb-kv/music-catalog-ms',
         'https://travis-ci.org/RSO-lb-kv/song-upload-ms',
+        'https://travis-ci.org/RSO-lb-kv/comments-ms',
       ],
       dockerhub: [
         'https://hub.docker.com/repository/docker/lbracun/music-catalog-ms',
         'https://hub.docker.com/repository/docker/lbracun/song-upload',
+        'https://hub.docker.com/repository/docker/klavdijav/comments-ms',
       ],
     };
   }
